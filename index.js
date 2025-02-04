bcc62ea70a7b07d37f521d077de333ca0e3bacec
ABazing <jessbaze@gmail.com>
2025-02-04 23:55:28 +0100

    -todo list

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('My code is running');
+const todoInput = document.getElementById('new-todo');
+const addButton = document.getElementById('add-todo');
+const todoList = document.getElementById('todo-list');
+
+addButton.addEventListener('click', addTodo);
+
+function addTodo() {
+  const todoText = todoInput.value.trim();
+  const deleteButton = document.createElement('button');
+  const newTodoItem = document.createElement('li');
+  if (todoText) {
+    newTodoItem.textContent = todoText;
+    newTodoItem.addEventListener('click', toggleComplete);
+
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', removeTodo);
+
+    newTodoItem.appendChild(deleteButton);
+    todoList.appendChild(newTodoItem);
+    todoInput.value = '';
+  }
+}
+
+function toggleComplete(event) {
+  event.target.classList.toggle('completed');
+}
+
+function removeTodo(event) {
+  const listItem = event.target.parentElement;
+  listItem.remove();
+}
